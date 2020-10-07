3d9c0d7d00a37575e999e23241177e4ea4525c25
Kezia Olive <keziaolive@Kezias-MacBook-Pro.local>
2020-10-07 10:15:15 +0200

    Updates

---
+++ precache-manifest.0d8376fd714af1f60519e146a704a1b0.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9c520446cdde68da44c8c929f4c396",
+    "url": "/FirstPublish/index.html"
+  },
+  {
+    "revision": "58b1c38dcfdf1f5ea0ab",
+    "url": "/FirstPublish/static/css/2.c13de588.chunk.css"
+  },
+  {
+    "revision": "2f1496faa12b46d3964f",
+    "url": "/FirstPublish/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58b1c38dcfdf1f5ea0ab",
+    "url": "/FirstPublish/static/js/2.c369a335.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/FirstPublish/static/js/2.c369a335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1496faa12b46d3964f",
+    "url": "/FirstPublish/static/js/main.caac1499.chunk.js"
+  },
+  {
+    "revision": "019de1e1b6bdfb72f7cf",
+    "url": "/FirstPublish/static/js/runtime-main.ccfb44b9.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/FirstPublish/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/FirstPublish/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/FirstPublish/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/FirstPublish/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/FirstPublish/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/FirstPublish/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/FirstPublish/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/FirstPublish/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/FirstPublish/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/FirstPublish/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/FirstPublish/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/FirstPublish/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/FirstPublish/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/FirstPublish/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/FirstPublish/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/FirstPublish/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
